ec9beb05380a210df62be8cbd850070822e1d7cf
Jr Bytes <itamazor@gmail.com>
2020-04-13 02:38:14 -0300

    Adicionado funcionalidade para liberar acesso para clientes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3333
 
 const routes = require('./routes')
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
